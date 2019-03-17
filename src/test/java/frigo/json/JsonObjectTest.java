4eca68942eaf2344bef65224a1de20f71e0598aa
Frigo <frigocoder@gmail.com>
2019-03-17 09:12:31 +0100

    Rephrased tests and removed null support for the moment.

--- src/test/java/frigo/json/JsonObjectTest.java
+++ src/test/java/frigo/json/JsonObjectTest.java
@@ -4,66 +4,51 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.sameInstance;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class JsonObjectTest {
 
-    private JsonObject json = new JsonObject();
+    private JsonObject obj1 = new JsonObject();
+    private JsonObject obj2 = new JsonObject();
+
+    private JsonObject json = new JsonObject()
+            .put("boolean1", true)
+            .put("boolean2", false)
+            .put("integer1", 1)
+            .put("integer2", 2)
+            .put("double1", 1.0)
+            .put("double2", 2.0)
+            .put("object1", obj1)
+            .put("object2", obj2)
+            .put("string1", "value1")
+            .put("string2", "value2");
 
     @Test
-    public void string_fields() {
-        json.put("key1", "value1").put("key2", "value2");
-        assertThat(json.get("key1"), is("value1"));
-        assertThat(json.get("key2"), is("value2"));
-        assertThat(json.getString("key1"), is("value1"));
-        assertThat(json.getString("key2"), is("value2"));
+    public void get_works_for_all_field_types() {
+        assertThat(json.get("boolean1"), is(true));
+        assertThat(json.get("boolean2"), is(false));
+        assertThat(json.get("integer1"), is(1));
+        assertThat(json.get("integer2"), is(2));
+        assertThat(json.get("double1"), is(1.0));
+        assertThat(json.get("double2"), is(2.0));
+        assertThat(json.get("object1"), sameInstance(obj1));
+        assertThat(json.get("object2"), sameInstance(obj2));
+        assertThat(json.get("string1"), is("value1"));
+        assertThat(json.get("string2"), is("value2"));
     }
 
     @Test
     public void getString_throws_for_non_strings() {
-        assertThrows(JsonException.class, () -> new JsonObject().put("key1", 1).getString("key1"));
-        assertThrows(JsonException.class, () -> new JsonObject().put("key1", 1.0).getString("key1"));
-        assertThrows(JsonException.class, () -> new JsonObject().put("key1", new JsonObject()).getString("key1"));
-        assertThrows(JsonException.class, () -> new JsonObject().put("key1", true).getString("key1"));
-        assertThrows(JsonException.class, () -> new JsonObject().put("key1", null).getString("key1"));
+        assertThrows(JsonException.class, () -> json.getString("boolean1"));
+        assertThrows(JsonException.class, () -> json.getString("boolean2"));
+        assertThrows(JsonException.class, () -> json.getString("integer1"));
+        assertThrows(JsonException.class, () -> json.getString("integer2"));
+        assertThrows(JsonException.class, () -> json.getString("double1"));
+        assertThrows(JsonException.class, () -> json.getString("double2"));
+        assertThrows(JsonException.class, () -> json.getString("object1"));
+        assertThrows(JsonException.class, () -> json.getString("object2"));
     }
 
-
-    @Test
-    public void integer_fields() {
-        json.put("key1", 1).put("key2", 2);
-        assertThat(json.get("key1"), is(1));
-        assertThat(json.get("key2"), is(2));
-    }
-
-    @Test
-    public void double_fields() {
-        json.put("key1", 1.0).put("key2", 2.0);
-        assertThat(json.get("key1"), is(1.0));
-        assertThat(json.get("key2"), is(2.0));
-    }
-
-    @Test
-    public void object_fields() {
-        JsonObject obj1 = new JsonObject();
-        JsonObject obj2 = new JsonObject();
-        json.put("key1", obj1).put("key2", obj2);
-        assertThat(json.get("key1"), is(obj1));
-        assertThat(json.get("key2"), is(obj2));
-    }
-
-    @Test
-    public void boolean_fields() {
-        json.put("key1", true).put("key2", false);
-        assertThat(json.get("key1"), is(true));
-        assertThat(json.get("key2"), is(false));
-    }
-
-    @Test
-    public void null_field() {
-        json.put("key1", null);
-        assertThat(json.get("key1"), nullValue());
-    }
 
 }
